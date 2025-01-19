7dadb961b0876b5b8b21239ee0f04f0d706f6c59
IsabellaSoares <isabella.soares.28@gmail.com>
2025-01-19 16:11:56 -0300

    feat: integration with balance endpoint

--- src/modules/home/components/header/index.tsx
+++ src/modules/home/components/header/index.tsx
@@ -1,24 +1,42 @@
 import { Header as UIHeader, Menu } from "fiap-financeiro-ds";
 import MenuItems from "../menu-items";
-import { useRecoilValue } from "recoil";
+import { useSetRecoilState } from "recoil";
 import { userState } from "@/recoil/atoms/userAtom";
-import type { User } from "@/types/user";
-import { useLogin } from "@/modules/hooks/useLogin.hook";
-import { useAccount } from "@/modules/hooks/useAccount.hook";
 import { useEffect } from "react";
+import useSWR from "swr";
+import { loginUserRequest } from "@/services/user";
+import { balanceRequest } from "@/services/balance";
+import { balanceState } from "@/recoil/atoms/balanceAtom";
 
 export default function Header() {
-  useLogin();
-  const { getAccount } = useAccount();
+  const setUser = useSetRecoilState(userState);
+  const setBalance = useSetRecoilState(balanceState);
 
-  const user = useRecoilValue<User>(userState);
+  const { data: userData, isLoading: userIsLoading } = useSWR(
+    "/user/1",
+    loginUserRequest
+  );
+  const { data: balanceData, isLoading: balanceIsLoading } = useSWR(
+    "/balance?userId=1",
+    balanceRequest
+  );
 
   useEffect(() => {
-    getAccount();
-  }, []);
+    if (!userIsLoading && userData) {
+      setUser(userData.data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userData, userIsLoading]);
+
+  useEffect(() => {
+    if (!balanceIsLoading && balanceData) {
+      setBalance(balanceData.data[0].currentAccount);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [balanceData, balanceIsLoading]);
 
   return (
-    <UIHeader isLogged userName={user.name || ""}>
+    <UIHeader isLogged userName={userData?.data?.name || ""}>
       <Menu variant="compact" iconColor="secondary">
         <MenuItems />
       </Menu>
