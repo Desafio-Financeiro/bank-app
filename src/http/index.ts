b03806f15785eaf18171a38c0bf3eb605f5c3a91
IsabellaSoares <isabella.soares.28@gmail.com>
2025-01-19 15:58:42 -0300

    feat: update api with new url

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL_FIAP_API,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export default http;
