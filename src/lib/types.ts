e0c38f26ccc1f6224d15a19954a76f2801a3e650
IsabellaSoares <isabella.soares.28@gmail.com>
2025-01-19 20:50:18 -0300

    refactor: delete unused components

--- src/lib/types.ts
+++
@@ -1,7 +0,0 @@
-import type { TransactionTypes } from "@/types/transaction";
-
-export interface EditTransacao {
-  id: string;
-  value: number;
-  type: TransactionTypes;
-}
--- src/modules/transactions/components/loading/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Typography } from "@mui/material";
-
-export function Loading() {
-  return <Typography>Carregando...</Typography>;
-}
