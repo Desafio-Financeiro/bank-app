151b86032d8e3c228f43f95ea7fb00f7a41c97d4
Cris <kristiano.gon@gmail.com>
2025-01-20 11:35:25 -0300

    chore: update configs

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ?? "https://json-server-api-mu.vercel.app",
 });
 
 export default http;
