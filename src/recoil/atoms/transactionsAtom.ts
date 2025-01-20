a1c2a7994d12b2d86344a7fe4697ceb264e11b6f
IsabellaSoares <isabella.soares.28@gmail.com>
2025-01-19 21:25:19 -0300

    refactor: delete unused atom

--- src/recoil/atoms/transactionsAtom.ts
+++
@@ -1,7 +0,0 @@
-import type { Transaction } from "@/types/transaction";
-import { atom } from "recoil";
-
-export const transactionsState = atom({
-  key: "transactionsState",
-  default: [] as Transaction[],
-});
